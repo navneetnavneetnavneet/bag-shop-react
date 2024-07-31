a6d22a7a6c58f233eff59f2dff0a7c5ea1bb1947
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-07-31 23:18:50 +0530

    create project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Register from "./components/register/Register";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
